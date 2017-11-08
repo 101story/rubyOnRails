5df36cef87be8ebf4b8ab3f3b4ee7f2eccfbc0af
Jinhee <101storyjh@daum.net>
2017-11-08 09:52:19 +0000

    first commit

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,52 @@
+class PostController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+    
+    def create
+        #erb:create 생략되어 있음 
+        @title = params[:title]
+        @content = params[:content]
+        
+        Post.create(
+            title:@title,
+            content:@content
+        )
+    end
+    
+    def show
+        @id = params[:id]
+        @post = Post.find(@id)
+        # @title = @post.title
+        # @content = @post.content
+    end
+    
+    def modify
+        @id = params[:id]
+        @post = Post.find(@id)
+    end
+    
+    def update
+        @id = params[:id]
+        @title = params[:title]
+        @content = params[:content]
+        post = Post.find(@id)
+        post.update(
+            title:@title,
+            content:@content
+        )
+        
+        redirect_to '/'
+    end
+    
+    def destroy
+        # 지울 글을 찾는다. 
+        @id = params[:id]
+        post = Post.find(@id)
+        # 글을 지운다. 
+        post.destroy
+        
+        redirect_to '/'
+    end
+    
+end
